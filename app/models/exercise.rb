e60cf80476ed23daaa7ccb70b6d28d88382bf5fc
rwparrish <rino3717@gmail.com>
2020-06-22 21:47:41 -0600

    modify validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,6 +2,6 @@ class Exercise < ApplicationRecord
     
     belongs_to :user
     belongs_to :workout
-    validates_presence_of :name
+    
 
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,5 +4,7 @@ class Workout < ApplicationRecord
     has_many :users, through: :exercises
     accepts_nested_attributes_for :exercises, reject_if: :all_blank
     validates_presence_of :name
+
+    scope :most_recent, -> { where('DATE(created_at) >= ?', 7.days.ago) }
    
 end
